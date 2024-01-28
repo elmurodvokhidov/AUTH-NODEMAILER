a8065ed44aaa0b2dc45f9b2eae5de8f5376d9d88
elmurod <elmurodvkh@gmail.com>
2024-01-28 21:59:11 +0500

    deleted home endpoint

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(bodyParser());
-app.use('/', (req, res) => {
-    res.send('Welcome to the Server Side! 👋')
-})
 app.use('/user', UserRouter);
 
 app.listen(PORT, () => {
